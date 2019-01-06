dab269e3896d6500e24748289df0b39302011947
Clint Bush <clint@madebyclint.com>
2019-01-06 14:44:07 -0600

    Test out render method

--- js/board-bar.js
+++ js/board-bar.js
@@ -12,4 +12,22 @@ t.render(function(){
   // and then called each time something changes that
   // you might want to react to, such as new data being
   // stored with t.set()
+  console.log('loaded');
+
+  function checkBoardLoad(element) {
+		console.log('checking if ' + element + ' is loaded');
+		var el = document.querySelector(element);
+		if (el) {
+			console.log(element + ' is loaded');
+			// addButton(el);
+			// runScript(el);	
+		} else {
+			console.log(element + ' not loaded');
+			setTimeout(function() {
+				checkBoardLoad(element);	
+			}, 500);
+		}
+	}
+	
+	checkBoardLoad('.board-header-btns.mod-right');	
 });
