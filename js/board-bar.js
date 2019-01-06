6646eefda16d194c6b19312fbc611f427bbc2e7e
Clint Bush <clint@madebyclint.com>
2019-01-06 15:05:56 -0600

    Pull lists

--- js/board-bar.js
+++ js/board-bar.js
@@ -12,4 +12,8 @@ t.render(function(){
   // and then called each time something changes that
   // you might want to react to, such as new data being
   // stored with t.set()
+  return t.lists('all')
+    .then(function (lists) {
+      console.log(JSON.stringify(lists, null, 2));
+    });
 });
